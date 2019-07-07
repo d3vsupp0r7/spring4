ee95c1867d49a2ed0f096f4ca9cf878cc0911760
d3vsupp0r7 <dev@dev.com>
2019-07-07 12:38:24 +0200

    Example for form validation - 1

--- spring4_mvc_helloworld/src/main/java/org/lba/spring4/controller/impl/EmployeeControllerImpl.java
+++ spring4_mvc_helloworld/src/main/java/org/lba/spring4/controller/impl/EmployeeControllerImpl.java
@@ -5,27 +5,75 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.lba.spring4.controller.EmployeeController;
 import org.lba.spring4.controller.data.EmployeeModel;
+import org.lba.spring4.db.model.EmployeeDBModel;
 import org.lba.spring4.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/employee-web")
 public class EmployeeControllerImpl implements EmployeeController{
-	
+
 	static final Logger logger = Logger.getLogger(EmployeeControllerImpl.class);
-	
+
 	@Autowired
 	private EmployeeService employeeService;
 
-	@Override
-	public EmployeeModel createEmployee(EmployeeModel aEmployee) {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping(value = "/addEmployee", method = RequestMethod.GET)
+	public String setupForm(Model model)
+	{
+		EmployeeModel employeeVO = new EmployeeModel();
+		model.addAttribute("employee", employeeVO);
+		return "employees/form/addEmployee";
 	}
 
 	@Override
-	public List<EmployeeModel> readAllEmployees() {
-		// TODO Auto-generated method stub
+	@RequestMapping(value = "/submitAddEmployee", method = RequestMethod.POST)
+	public String createEmployee(@ModelAttribute("employee") EmployeeModel aEmployee, BindingResult result) {
+
+		//Validation code start
+		boolean error = false;
+		//Verifying if information is same as input by user  
+		logger.debug("Employee form data: " + aEmployee.toString()); 
+
+		error = validateEmployeeForm(aEmployee, result);
+		
+		if(error) {
+			return "employees/form/addEmployee";
+		}
+
+		return "employees/form/addSuccess";
+	}
+
+	private boolean validateEmployeeForm(EmployeeModel aEmployee,  BindingResult result) {
+
+		boolean error = false;
+		
+		if(aEmployee.getName().isEmpty()){
+			result.rejectValue("name", "error.name");
+			error = true;
+		}
+
+		if(aEmployee.getSurname().isEmpty()){
+			result.rejectValue("surname", "error.surname");
+			error = true;
+		}
+		
+		return error;
+	}
+
+	@Override
+	@GetMapping("/listAllEmployee")
+	public List<EmployeeModel> readAllEmployees(@ModelAttribute("employee") EmployeeModel user, Model model) {
+
+		List<EmployeeDBModel> employeeFromDB = employeeService.getAllEmployees();
+
 		return null;
 	}
 
@@ -41,5 +89,5 @@ public class EmployeeControllerImpl implements EmployeeController{
 		return null;
 	}
 
-	
+
 }
