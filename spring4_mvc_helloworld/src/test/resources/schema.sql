7bc214975814caa045dc932394a41d827dcc5f73
d3vsupp0r7 <d3vsupp0r7@example.com>
2019-07-21 22:30:23 +0200

    JUnit - 1

--- spring4_mvc_helloworld/src/test/resources/schema.sql
+++
@@ -1,9 +0,0 @@
--- EMPLOYEE TABLE
-create table if not exists employee(
-
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   name varchar(255) not null,
-   surname varchar(255) not null,
-   primary key(id)
-
-);
