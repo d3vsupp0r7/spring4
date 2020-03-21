6d354be0f941baa2d2dc3954ea90c45d47219b3e
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-03-21 09:36:44 +0100

    JUnit - baseline - 1

--- spring4_batch_advanced/src/test/java/org/lba/spring4/crud/integration/tests/H2DatabaseCrudOperationTests.java
+++ spring4_batch_advanced/src/test/java/org/lba/spring4/crud/integration/tests/H2DatabaseCrudOperationTests.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.lba.spring4.model.EmployeeModel;
+import org.lba.spring4.model.Employee;
 import org.lba.spring4.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,28 +18,29 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-        "classpath:/springTestContext/crud/spring-test-context-crud_with_initial_load.xml"})
+"classpath:/springTestContext/crud/spring-test-context-crud_with_initial_load.xml"})
 public class H2DatabaseCrudOperationTests {
 
 	final static Logger logger = Logger.getLogger(H2DatabaseCrudOperationTests.class);
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	@Test
 	public void testSpringContextLoading() {
 		logger.debug("** TEST testSpringContextLoading() - START **");
 		logger.debug("A sample test for checking the spring test context bean build correctly");
-		employeeService.sayHello();
+	//	employeeService.sayHello();
 		logger.debug("** TEST testSpringContextLoading() - START **");
 	}
-	
+
+	/* CREATE */
 	@Test 
 	public void saveEmployee() {
 		logger.debug("** TEST saveEmployee - START **");
-		EmployeeModel model = getTestEmployeeModel();
+		Employee model = getTestEmployeeModel();
 		/**/
-		EmployeeModel savedEmployee = employeeService.saveModelObject(model);
+		Employee savedEmployee = employeeService.saveEmployee(model);
 		/**/
 		logger.debug("Saved Employee ID: " + savedEmployee.getId());
 		logger.debug("Saved Employee NAME: " + savedEmployee.getName());
@@ -52,23 +53,36 @@ public class H2DatabaseCrudOperationTests {
 		/**/
 		logger.debug("** TEST saveEmployee -   END **");
 	}
-	
+
+	/* READ */
 	@Test 
 	public void readEmployees() {
-		
+
 		logger.debug("** TEST readEmployees - START **");
-		
-		List<EmployeeModel> listEmployee = employeeService.getAllEmployee();
+
+		List<Employee> listEmployee = employeeService.listAllEmployees();
 		logger.debug("Employees size: " + listEmployee.size());
 		assertNotNull(listEmployee);
-		
+
 		logger.debug("** TEST readEmployees -   END **");
 	}
-	
-	private EmployeeModel getTestEmployeeModel() {
-		EmployeeModel employeeModel = new EmployeeModel();
+
+	@Test 
+	public void readEmployeeById() {
+
+		logger.debug("** TEST readEmployees - START **");
+
+		Employee employee = employeeService.findById(1l);
+		logger.debug("Employees size: " + employee.toString());
+		assertNotNull(employee);
+
+		logger.debug("** TEST readEmployees -   END **");
+	}
+
+	private Employee getTestEmployeeModel() {
+		Employee employeeModel = new Employee();
 		/**/
-		employeeModel.setId(1);
+		employeeModel.setId(1l);
 		employeeModel.setName("aNameEmployee");
 		employeeModel.setSurname("aSurnameEmployee");
 		employeeModel.setBornDate(new Date());
