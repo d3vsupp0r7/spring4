9b0e6ba4c7f5a5137c8bd9e9fdcdc245562ebbdf
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-12-01 14:04:28 +0100

    CustomMetadata update for channel name

--- spring4_standalone_springintegration/src/main/java/org/lba/spring4/integration/payload/CustomMetadata.java
+++ spring4_standalone_springintegration/src/main/java/org/lba/spring4/integration/payload/CustomMetadata.java
@@ -9,6 +9,7 @@ public class CustomMetadata implements Serializable {
 	 */
 	private static final long serialVersionUID = -4672179028947001658L;
 	private int maxMessages;
+	private String channelName;
 	
 	public CustomMetadata() {
 		// Implement if necessary
@@ -21,10 +22,18 @@ public class CustomMetadata implements Serializable {
 	public void setMaxMessages(int maxMessages) {
 		this.maxMessages = maxMessages;
 	}
+	
+	public String getChannelName() {
+		return channelName;
+	}
 
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+	
 	@Override
 	public String toString() {
-		return "CustomMetadata [maxMessages=" + maxMessages + "]";
+		return "CustomMetadata [maxMessages=" + maxMessages + ", channelName=" + channelName + "]";
 	}
 	
 }
